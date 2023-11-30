2c2d430c0094094424513395241097052d3970db
LiosK <contact@mail.liosk.net>
2023-12-01 08:54:03 +0900

    test: add writer_with_handler_zero_copy_ub bench

--- benches/replace.rs
+++ benches/replace.rs
@@ -1,6 +1,7 @@
 //! Compares the throughput of replacing malformed bytes and unmappable characters.
 
 #![feature(test)]
+#![feature(maybe_uninit_slice)]
 
 extern crate test;
 
@@ -120,6 +121,51 @@ fn writer_with_handler(b: &mut test::Bencher) {
         }
 
         assert_eq!(writer.writer_ref(), &expected);
+    });
+}
+
+/// Writes directly into the spare capacity of a `Vec<u8>` in an UB manner.
+#[bench]
+fn writer_with_handler_zero_copy_ub(b: &mut test::Bencher) {
+    struct UBBuffer(Vec<u8>);
+    impl io::Write for UBBuffer {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            self.0.write(buf)
+        }
+        fn flush(&mut self) -> io::Result<()> {
+            self.0.flush()
+        }
+    }
+    impl encoding_rs_rw::misc::BufferedWrite for UBBuffer {
+        fn unfilled(&mut self) -> &mut [u8] {
+            // SAFETY: UB! `assume_init` an uninitialized buffer is UB even if T is `u8`
+            unsafe { std::mem::MaybeUninit::slice_assume_init_mut(self.0.spare_capacity_mut()) }
+        }
+        fn advance(&mut self, n: usize) {
+            unsafe { self.0.set_len(self.0.len() + n) };
+        }
+        fn try_reserve(&mut self, minimum: usize, size_hint: Option<usize>) -> io::Result<()> {
+            let size_hint = size_hint.unwrap_or(minimum);
+            if size_hint > minimum && self.0.try_reserve(size_hint).is_ok() {
+                return Ok(());
+            }
+            self.0
+                .try_reserve(minimum)
+                .map_err(|e| io::Error::new(io::ErrorKind::OutOfMemory, e))
+        }
+    }
+
+    let src = test::black_box(TEXT);
+    let expected = encoder_expected(TEXT);
+    b.iter(|| {
+        let mut writer = EncodingWriter::with_buffer(UBBuffer(Vec::new()), Enc.new_encoder());
+        {
+            let mut writer = writer.with_unmappable_handler(|e, w| write_ncr(e.value(), w));
+            write!(writer, "{}", src).unwrap();
+            writer.flush().unwrap();
+        }
+
+        assert_eq!(&writer.buffer_ref().0, &expected);
     });
 }
 
